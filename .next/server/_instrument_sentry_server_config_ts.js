e1ae182ba23d941d538a837e12a58079370a5694
PrathamBarvalta <prathambar02@gmail.com>
2024-06-06 19:33:56 +0530

    tracessamplerate

---
+++ .next/server/_instrument_sentry_server_config_ts.js
@@ -0,0 +1,24 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_instrument_sentry_server_config_ts";
+exports.ids = ["_instrument_sentry_server_config_ts"];
+exports.modules = {
+
+/***/ "(instrument)/./sentry.server.config.ts":
+/*!*********************************!*\
+  !*** ./sentry.server.config.ts ***!
+  \*********************************/
+/***/ (() => {
+
+eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIoaW5zdHJ1bWVudCkvLi9zZW50cnkuc2VydmVyLmNvbmZpZy50cyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(instrument)/./sentry.server.config.ts\n");
+
+/***/ })
+
+};
+;
